ba85678ef38fc4076a85ff972e67b3afcc9c439b
Remy Schrader <remy@rootandflow.com>
2016-10-08 12:51:37 -0700

    converts to function; check config to save changes to app settings

--- script/modules/iterm2.sh
+++ script/modules/iterm2.sh
@@ -1,19 +1,44 @@
 #!/bin/bash
 #
-## iTerm2
 
-# Install monospace Font for Powerline 
-[[ -r "${HOME}/Library/Fonts/Inconsolata-dz for Powerline.otf" ]]\
-  || brew bundle --file=- <<EOF
-    cask 'font-inconsolata-dz-for-powerline'
-EOF
+config_iTerm2 () {
 
-# Load config from directory; set path to plist file. 
-defaults write com.googlecode.iterm2 \
-  LoadPrefsFromCustomFolder \
-  -int 1
+  local DEFAULT="${HOME}/pro/src/github.com/remy-actual/remdot/config"
+  local CONFIG="${REMDOT_CONFIG_DIR:-$DEFAULT}/iterm2"
 
-defaults write com.googlecode.iterm2 \
-  PrefsCustomFolder \
-  -string "${REMDOT_CONFIG_DIR}/iterm2"
+  # Install monospace Font for Powerline 
+  log "Configuring iTerm2..."
+  logn "Checking for Powerline font..."
+  [[ -r "${HOME}/Library/Fonts/Inconsolata-dz for Powerline.otf" ]]\
+    || brew bundle --file=- <<-EOF
+      cask 'font-inconsolata-dz-for-powerline'
+	EOF
+  logk
+
+  logn "Load settings from plist file in Remdot repository..."
+  defaults write com.googlecode.iterm2 \
+    LoadPrefsFromCustomFolder \
+    -bool TRUE
+
+  defaults write com.googlecode.iterm2 \
+    PrefsCustomFolder \
+    -string "${CONFIG}"
+  logk
+
+  logn "Set app to save config changes on exit..."
+  if [[ $(defaults read com.googlecode.iterm2 \
+                        NoSyncNeverRemindPrefsChangesLostForFile_selection) != 0 ]]
+  then
+    defaults write com.googlecode.iterm2 \
+      NoSyncNeverRemindPrefsChangesLostForFile \
+      -bool TRUE
+    defaults write com.googlecode.iterm2 \
+      NoSyncNeverRemindPrefsChangesLostForFile_selection \
+      -bool FALSE
+  fi
+  logk
+
+}
+
+config_iTerm2
 
